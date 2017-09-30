fc576d8f3232d5cfc69dd2dd51667cebc332d11c
Amit Mundra <amit.mundra52@gmail.com>
2017-10-01 02:48:05 +0530

    not used here

--- navbar-module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-require('angular-material');
-var navbarCtrl = require('./navbar.js');
-var navbarModule = angular.module('myApp.shared',['ngRoute','ngMaterial']);
-navbarModule.controller('navbarCtrl',navbarCtrl);
